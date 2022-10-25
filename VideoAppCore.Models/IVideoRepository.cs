a75bc023859f04cb82f69e09c2717f1990d6ed90
youngil <youngil@placeb.com>
2022-10-25 09:50:34 +0900

    IVideoRespository 인터페이스 생성

---
+++ VideoAppCore.Models/IVideoRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace VideoAppCore.Models
+{
+    public interface IVideoRepository
+    {
+        Video AddVideo(Video video);
+        List<Video> GetAll();
+        Video GetById(int id);
+        Video UpdateVideo(Video video);
+        void RemoveVideo(int id);
+    }
+}
