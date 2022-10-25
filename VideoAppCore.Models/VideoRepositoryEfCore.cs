72dc31ad58b8ead587f662482e1494b83fa12cc2
youngil <youngil@placeb.com>
2022-10-25 21:11:52 +0900

    IVideoRepository를 상속받는 Repository 클래스 추가

---
+++ VideoAppCore.Models/VideoRepositoryEfCore.cs
@@ -0,0 +1,30 @@
+﻿namespace VideoAppCore.Models
+{
+    public class VideoRepositoryEfCore : IVideoRepository
+    {
+        public Video AddVideo(Video video)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Video> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Video GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveVideo(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Video UpdateVideo(Video video)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
