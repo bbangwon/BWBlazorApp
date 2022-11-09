a620b91f1de3abddd08ab3f6cfcc7b1bd4e3466d
youngil <youngil@placeb.com>
2022-11-09 21:29:05 +0900

    Manufacturer Repository 구현

---
+++ ManufacturerApp/Models/IManufacturerRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ManufacturerApp.Models
+{
+    public interface IManufacturerRepository
+    {
+        Task<Manufacturer> Add(Manufacturer manufacturer);
+        Task<List<Manufacturer>?> GetAll();
+        Task<Manufacturer?> GetById(int id);
+        Task<Manufacturer?> Edit(Manufacturer manufacturer);
+        Task Delete(int id);
+    }
+}
---
+++ ManufacturerApp/Models/ManufacturerRepository.cs
@@ -0,0 +1,60 @@
+﻿using ManufacturerApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManufacturerApp.Models
+{
+    public class ManufacturerRepository : IManufacturerRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public ManufacturerRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Manufacturer> Add(Manufacturer manufacturer)
+        {
+            this.dbContext.Add(manufacturer);
+            await this.dbContext.SaveChangesAsync();
+            return manufacturer;
+        }
+        public async Task<List<Manufacturer>?> GetAll()       
+        {
+            if (this.dbContext.Manufacturers == null)
+                return null;
+
+            return await this.dbContext.Manufacturers.OrderBy(m => m.Id).ToListAsync();
+        }
+
+        public async Task<Manufacturer?> GetById(int id)
+        {
+            if (this.dbContext.Manufacturers == null)
+                return null;
+
+            return await this.dbContext.Manufacturers.FindAsync(id);
+        }
+        public async Task<Manufacturer?> Edit(Manufacturer manufacturer)
+        {
+            if (this.dbContext.Manufacturers == null)
+                return null;
+
+            this.dbContext.Manufacturers.Update(manufacturer);
+            await this.dbContext.SaveChangesAsync();
+
+            return manufacturer;
+        }
+
+        public async Task Delete(int id)
+        {
+            var manufacturer = await GetById(id);
+
+            if (manufacturer == null)
+                return;
+
+            this.dbContext.Manufacturers?.Remove(manufacturer);
+            await this.dbContext.SaveChangesAsync();
+        }
+
+
+    }
+}
