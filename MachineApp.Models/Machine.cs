6704d832a2bd68debfeafdf64cb1d0d7923811df
youngil <youngil@placeb.com>
2022-11-16 09:51:35 +0900

    Machine Repository Interface 생성

--- MachineApp.Models/Machine.cs
+++ MachineApp.Models/Machine.cs
@@ -10,4 +10,46 @@ namespace MachineApp.Models
         [Required]
         public string? Name { get; set; }
     }
+
+    public interface IMachineRepository
+    {
+        Task<Machine> AddMachineAsync(Machine machine); 
+        Task<List<Machine>> GetMachinesAsync();
+        Task<Machine> GetMachineByIdAsync(int id);
+        Task<Machine> EditMachineAsync(Machine machine);
+        Task DeleteMachineAsync(int id);        
+    }
+
+    public class MachineRepository : IMachineRepository
+    {
+        public Task<Machine> AddMachineAsync(Machine machine)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMachineAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Machine> EditMachineAsync(Machine machine)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Machine> GetMachineByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Machine>> GetMachinesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MachineDbContext
+    {
+
+    }
 }
