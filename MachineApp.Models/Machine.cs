b629770a7cfc4fb97ffe9aaa6d385b13658e9997
youngil <youngil@placeb.com>
2022-11-17 09:54:15 +0900

    Machine EFCore 설정

--- MachineApp.Models/Machine.cs
+++ MachineApp.Models/Machine.cs
@@ -1,4 +1,6 @@
 ﻿using MachineApp.Models.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols;
 using System.ComponentModel.DataAnnotations;
 
 namespace MachineApp.Models
@@ -48,8 +50,37 @@ namespace MachineApp.Models
         }
     }
 
-    public class MachineDbContext
+    public class MachineDbContext : DbContext
     {
+        private readonly string? connectionString;
 
+        public MachineDbContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public MachineDbContext(DbContextOptions<MachineDbContext> options) 
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(connectionString);  
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Machine>()
+                .Property(m => m.Created)
+                .HasDefaultValueSql("GetDate()");
+        }
+
+        public DbSet<Machine>? Machines { get; set; }
     }
 }
